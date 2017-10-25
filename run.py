4fbc1986a9efa1376a9e01f5339cd0d155663ec3
Heejong Park <hpar081@aucklanduni.ac.nz>
2017-10-26 11:08:59 +1300

    small update

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def main():
     x = SysJInput('localhost', 2000, inputHandler)
     sjout = SysJOutput('localhost', 1000)
     while True:
-        value = bytearray([1,1,2,2,3,3])
+        value = bytearray([20,200,10,200,1,3])
         sjout.send(0x0A, TYPE_THL, value)
         time.sleep(2)
         sjout.send(0x1E, TYPE_POWER, "\x02\xFF")
